82a4f39af4354f2fe8eb27e38b9fc1c3e8000030
PlannedCompanies-DevTeam <DevOps@PlannedCompanies.com>
2019-05-07 14:14:44 -0400

    Add project files.

---
+++ app/App_Code/Handlers/Blob.generated.cs
@@ -0,0 +1,17 @@
+﻿namespace MyCompany.Handlers
+{
+    
+    
+    public partial class BlobFactoryConfig : BlobFactory
+    {
+        
+        public static void Initialize()
+        {
+            // register blob handlers
+            RegisterHandler("CheckRequests_AttachmentsAttachment", "\"dbo\".\"CheckRequests_Attachments\"", "\"Attachment\"", new string[] {
+                        "\"id\""}, "Checkrequests Attachments Attachment", "CheckrequestsAttachments", "Attachment");
+            RegisterHandler("SiteContentData", "\"dbo\".\"SiteContent\"", "\"Data\"", new string[] {
+                        "\"SiteContentID\""}, "Site Content Data", "SiteContent", "Data");
+        }
+    }
+}
---
+++ app/App_Code/Rules/CheckRequests.Generated.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Security;
+using MyCompany.Data;
+
+namespace MyCompany.Rules
+{
+	public partial class CheckRequestsBusinessRules : MyCompany.Rules.SharedBusinessRules
+    {
+        
+        [ControllerAction("CheckRequests", "Insert", ActionPhase.Before)]
+        [ControllerAction("CheckRequests", "Update", ActionPhase.Before)]
+        public void AssignFieldValuesToCheckRequests(
+                    int? chkreqID, 
+                    string chkreqStatus, 
+                    int? chkreqSendtoUserid, 
+                    int? chkreqRequestedbyUserid, 
+                    string chkreqPickup, 
+                    int? chkreqReasonID, 
+                    string chkreqOffice, 
+                    decimal? chkreqAmount, 
+                    string chkreqReasoncrReason, 
+                    string chkreqJobno, 
+                    int? chkreqApprovedbyUserid, 
+                    DateTime? chkreqApproveddate, 
+                    string chkreqToName, 
+                    string chkreqToLine1, 
+                    string chkreqToLine2, 
+                    string chkreqtoLine3, 
+                    string chkreqToCity, 
+                    string chkreqToState, 
+                    string chkreqToZipcode, 
+                    DateTime? createdDate, 
+                    DateTime? updatedDate, 
+                    string employeeSearch)
+        {
+            var ChkreqPickupFieldValue = SelectFieldValueObject("ChkreqPickup");
+            var ChkreqPickupCodeValue = 'P';
+            if (ChkreqPickupFieldValue == null)
+            	AddFieldValue("ChkreqPickup", ChkreqPickupCodeValue);
+            else
+            {
+                ChkreqPickupFieldValue.NewValue = ChkreqPickupCodeValue;
+                ChkreqPickupFieldValue.Modified = true;
+                ChkreqPickupFieldValue.ReadOnly = false;
+            }
+        }
+    }
+}
